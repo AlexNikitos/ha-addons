6a8773928e89305a93adb4710d40e352008787cc
Alexey Nikitin <alexnikitos@MacBook-Pro-Alexey-2.local>
2025-07-19 19:18:24 +0300

    Удалена лишняя генерация options.json

--- motionblinds_mqtt/run.sh
+++ motionblinds_mqtt/run.sh
@@ -1,20 +1,4 @@
 #!/bin/sh
 
-echo "🔧 Генерация options.json из переменных окружения..."
-
-# Записываем параметры из переменных окружения в options.json
-cat <<EOF > /data/options.json
-{
-  "mqtt_broker": "$MQTT_BROKER",
-  "mqtt_port": $MQTT_PORT,
-  "mqtt_user": "$MQTT_USER",
-  "mqtt_password": "$MQTT_PASSWORD",
-  "mqtt_topic_state": "$MQTT_TOPIC_STATE",
-  "mqtt_topic_command": "$MQTT_TOPIC_COMMAND",
-  "api_key": "$API_KEY",
-  "blinds": $BLINDS
-}
-EOF
-
 echo "🚀 Запускаем motionblinds_mqtt.py..."
 python3 /app/motionblinds_mqtt.py
